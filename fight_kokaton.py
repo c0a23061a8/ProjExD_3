75bfd07dacbc2a66496185add8b1beae900299ba
c0a23061 <c0a23061a8@edu.teu.ac.jp>
2024-12-03 15:40:18 +0900

    マージコミット時の不整合の解消

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -160,22 +160,24 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bomb is not None:
-            if bird.rct.colliderect(bomb.rct):    
-                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                font = pg.font.Font(None, 80)
-                txt = font.render("Game Over", True,(255, 0, 0))
-                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
-                bird.change_img(8, screen)
-                pg.display.update()
-                time.sleep(1)
-                return
-        if (bomb is not None) and (beam is not None):
-            if bomb.rct.colliderect(beam.rct):  # 爆弾とビームが衝突したら
-                bird.change_img(6, screen)
-                pg.display.update()
-                beam = None
-                bomb = None
+        for bomb in bombs:
+            if bomb is not None:
+                if bird.rct.colliderect(bomb.rct):    
+                    # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                    font = pg.font.Font(None, 80)
+                    txt = font.render("Game Over", True,(255, 0, 0))
+                    screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
+                    bird.change_img(8, screen)
+                    pg.display.update()
+                    time.sleep(1)
+                    return
+        for i in range(len(bombs)):
+            if beam is not None:
+                if bombs[i].rct.colliderect(beam.rct):  # 爆弾とビームが衝突したら
+                    bird.change_img(6, screen)
+                    pg.display.update()
+                    beam = None
+                    bombs[i] = None
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
